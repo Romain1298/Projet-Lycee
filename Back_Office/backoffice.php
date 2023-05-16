92fadb216ed61f85bb08e87fff122044f3098fd6
DuDuch23 <109027799+DuDuch23@users.noreply.github.com>
2023-05-16 11:56:55 +0200

    Add files via upload

--- Back_Office/backoffice.php
+++ Back_Office/backoffice.php
@@ -14,8 +14,9 @@ if (isset($_POST['delete']))
             $delete = $con->prepare('DELETE FROM message WHERE id_message = :id');
             $delete->bindParam(':id', $formulaire['id']);
             $delete->execute();
-
-        } catch (\Exception $exception) {
+        } 
+        catch (\Exception $exception) 
+        {
             var_dump($exception);
         }
     }
@@ -32,29 +33,41 @@ if (isset($_POST['delete']))
     <link rel="stylesheet" href="backoffice.css">
 </head>
 <body>
-    <div id="content-modal">
-        <div id="modal-delete">
-            <h1 id="titre-modal-delete">Etes vous sur de vouloir supprimer cette ligne ?</h1>
-            <div id="button-oui-non">
-                <form action="#" method="POST">
-                    <input type="hidden" name="delete[nom]" value="<?php echo $liste["nom_message"] ?>">
-                    <input type="hidden" name="delete[id]" value="<?php echo $liste["id_message"] ?>">
-                    <button id="button-oui" onclick="">Oui</button>
-                </form>
-                <form action="#" method="POST">
-                    <button id="button-non" onclick="">Non</button>
-                </form>
+    <?php 
+        if (isset($_POST['predelete'])) 
+        {
+            $iddelete = $_POST['predelete'];
+            ?>
+            <div id="content-modal">
+                    <div id="modal-delete">
+                        <h1 id="titre-modal-delete">Etes vous sur de vouloir supprimer cette ligne ?</h1>
+                        <div id="button-oui-non">         
+                        <form action="#" method="POST">
+                            <input type="hidden" name="delete[id]" value="<?php echo $iddelete['id'] ?>">
+                            <button id="button-oui" onclick="">Oui</button>
+                        </form>
+                        <button id="button-non" onclick="hideModal()">Non</button>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <nav>
-        <ul>
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Partenariats</a></li>
-            <li><a href="#">Billeterie</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
+    <?php
+        } ?>
+    
+    <header>
+        <div id="font-bleu">
+                <div id="menu-hamburger">
+                    <span class="trait"></span>
+                    <span class="trait"></span>
+                    <span class="trait"></span>
+                </div>
+                <nav>
+                    <li id="accueil"><a href="#">Accueil</a></li>
+                    <li id="partenariats"><a href="#">Partenariats</a></li>
+                    <li id="billeterie"><a href="#">Billeterie</a></li>
+                    <li id="li-contact"><a href="#">Contact</a></li>
+                </nav>
+            </div>
+    </header>
 
     <table>
         <thead>
@@ -63,7 +76,7 @@ if (isset($_POST['delete']))
                 <th style="width: 10%">Nom</th>
                 <th style="width: 10%">Prénom</th>
                 <th style="width: 25%;">E-Mail</th>
-                <th>Contenu</th>
+                <th id="contenu">Contenu</th>
                 <th>Action</th>
             </tr>
         </thead>
@@ -78,7 +91,10 @@ if (isset($_POST['delete']))
                 <td style="width: 25%"><?php echo $liste['adresse_mail_message'] ?></td>
                 <td><?php echo $liste['contenu_message'] ?></td>
                 <td>
-                    <button id="delete" onclick="modal()" >SUPPRIMER</button>
+                    <form action="#" method="POST">
+                    <input type="hidden" value="<?php echo $liste['id_message'] ?>" name= "predelete[id]">
+                        <button id="delete" onclick="modal()" >SUPPRIMER</button>
+                    </form>
                 </td>
             </tr>
             <?php } ?>
@@ -89,6 +105,10 @@ if (isset($_POST['delete']))
         function modal(){
             var div = document.getElementById("content-modal")
             div.style.display = "flex";
+        }
+        function hideModal(){
+            var div = document.getElementById("content-modal")
+            div.style.display = "none";
         }
     </script>
 
